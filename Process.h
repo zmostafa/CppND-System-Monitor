ad471d57eca643772619b07408d05e8a55a05c40
Mostafa Ziad FRD XAXE13 <ziad.mostafa@zf.com>
2019-06-18 18:25:41 +0200

    add some getters

--- Process.h
+++ Process.h
@@ -43,6 +43,27 @@ void Process::setPid(int pid){
 string Process::getPid()const {
     return this->pid;
 }
+
+string Process::getUser()const {
+    return this->user;
+}
+
+string Process::getCmd()const {
+    return this->cmd;
+}
+
+int Process::getCpu() const {
+    return stoi(this->cpu);
+}
+
+int Process::getMem() const {
+    return stoi(this->mem);
+}
+
+string Process::getUpTime() const {
+    return this->upTime;
+}
+
 string Process::getProcess(){
     if(!ProcessParser::isPidExisting(this->pid))
         return "";
@@ -50,5 +71,5 @@ string Process::getProcess(){
     this->upTime = ProcessParser::getProcUpTime(this->pid);
     this->cpu = ProcessParser::getCpuPercent(this->pid);
 
-    return (this->pid + "   " + this->user + "   " + this->mem + "   " + this->cpu + "   " + this->upTime + "   " + this->cmd) ;//TODO: finish the string! this->user + "   "+ mem...cpu...upTime...;
+    return (this->pid + "   " + this->user + "   " + this->mem.substr(0,5) + "   " + this->cpu.substr(0,5) + "   " + this->upTime.substr(0,5) + "   " + this->cmd.substr(0,5)) ;//TODO: finish the string! this->user + "   "+ mem...cpu...upTime...;
 }
